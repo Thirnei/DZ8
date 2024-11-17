7e6a8f6e0cf3713c546e3d7778ff42b8d9ff5380
Danil Krasauskas <Thirnei@yandex.ru>
2024-11-17 12:08:12 +0200

    Initial1

---
+++ server.js
@@ -0,0 +1,7 @@
+const math = require('./math');
+
+const resultAdd = math.add(5, 3);
+const resultSubtract = math.subtract(5, 3);
+
+console.log(`Результат сложения: ${resultAdd}`);
+console.log(`Результат вычитания: ${resultSubtract}`);
